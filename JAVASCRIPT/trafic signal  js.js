644f75ab74078b3fcfa31866c83c2b76df6015dc
aasifashraf <aasifashraf786@gmai.com>
2022-12-20 14:19:04 +0530

    added javascript traffic signal lines

---
+++ JAVASCRIPT/trafic signal  js.js
@@ -0,0 +1,10 @@
+var light = "green"; 
+if (light = "green"){
+	console.log("you can go")
+} else if (light = "orange") {
+console.log("wait for the green light")
+} else if (light = "red") {
+	console.log("stop here untill light turns to red")
+} else{ 
+	console.log("Light is not working please pass the road carefully")
+	
