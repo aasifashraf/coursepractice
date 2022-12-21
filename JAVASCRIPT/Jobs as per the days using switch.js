e2cc359a85898ed132ce98fc0064c96794196bff
aasifashraf <aasifashraf786@gmai.com>
2022-12-21 12:15:42 +0530

    javascript switch and  if else statments

---
+++ JAVASCRIPT/Jobs as per the days using switch.js
@@ -0,0 +1,24 @@
+var days = 'Thursday';
+switch(days) {
+    case 'Monday':
+        console.log("work Monday");
+        break;
+    case "Tuesday":
+        console.log("work tuesday");
+        break;
+    case "Wednesday":
+        console.log("work wednesday")
+        break;
+    case "Thursday":
+        console.log("work thursday");
+        break;
+    case "Friday":
+        console.log("work friday");
+        break;
+    case "saturday":
+        console.log("work saturday");
+        break;
+    case "Sunday":
+        console.log("Holyday");
+        break;
+}
---
+++ JAVASCRIPT/defining the variable in js.js
@@ -0,0 +1,10 @@
+var petDog = "Rex"
+var petCat = "Pepper"
+console.log(petDog)
+console.log(petCat)
+console.log("My pet Dog's name is", petDog)
+console.log("My pet cat's name is", petCat)
+var catSound = "purr"
+var dogSound = "wuff"
+console.log(petDog,"says",dogSound)
+console.log(petCat,"says",catSound)
